838518b6c71a87a3f4b7c2b2e34dc14532601b68
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2023-05-08 14:28:42 +0330

    Remove some test methods

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,9 @@
 #include <QQmlApplicationEngine>
 
 #include "qtquick/cpp/tonqtquick.h"
-#include "core/wallet/tonwallet.h"
 
 int main(int argc, char *argv[])
 {
-    TON::Wallet::TonWallet t;
-    t.init("/home/bardia/Desktop/test", [&t](bool stt, const TON::Wallet::TonWallet::Error &err){
-        qDebug() << stt << err.message;
-//        t.createNewKey([](const QString &pkey, const TON::Wallet::TonWallet::Error &err){
-//            qDebug() << pkey;
-//        });
-
-        for (const auto &k: t.keys())
-        {
-            t.exportKey(k, [](const QStringList &words, const TON::Wallet::TonWallet::Error &err){
-                qDebug() << words;
-            });
-            t.getAddress(k, [k](const QString &words, const TON::Wallet::TonWallet::Error &err){
-                qDebug() << k << words;
-            });
-        }
-    });
-
     qputenv("QT_ANDROID_ENABLE_WORKAROUND_TO_DISABLE_PREDICTIVE_TEXT", "1");
     qputenv("QT_LOGGING_RULES", "qt.qml.connections=false");
 
